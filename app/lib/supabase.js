d7727639faf8a010cfc9bcd18a63f08dc1ecb5c6
syafar <target44135@gmail.com>
2024-12-20 09:03:40 +0800

    fix bug

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'default-url';
+const SUPABASE_ANON_KEY =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'default-key';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
